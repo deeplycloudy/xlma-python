b2e676e41f4a5ddc6d327aebd041e5e7248c563a
Eric Bruning <eric.bruning@gmail.com>
2022-06-24 12:40:44 -0500

    Tweaks to plot style and return artists from plotting functions

--- pyxlma/plot/xlma_base_plot.py
+++ pyxlma/plot/xlma_base_plot.py
@@ -10,9 +10,11 @@ import cartopy.feature as cfeature
 import cartopy.io.shapereader as shpreader
 from matplotlib.axes import Axes
 from cartopy.mpl.geoaxes import GeoAxes
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+
 GeoAxes._pcolormesh_patched = Axes.pcolormesh
 
-proj_cart = ccrs.PlateCarree(central_longitude=-95)
+# proj_cart = ccrs.PlateCarree(central_longitude=-95)
 
 # # Add a note about plotting counties by default if metpy is available in docs, and how to add your own map data without relying on built-ins.
 # reader = shpreader.Reader('UScounties/UScounties.shp')
@@ -85,7 +87,7 @@ class FractionalSecondFormatter(Formatter):
 class BlankPlot(object):
     """
     Generate a matching plot setup with no data
-    
+
     Requires:
     stime = starting time
     xlim, ylim = bounds of the domain
@@ -101,12 +103,12 @@ class BlankPlot(object):
         self.xlim = kwargs['xlim']
         self.bkgmap = bkgmap
         self.stime = stime
-        self.majorFormatter = FormatStrFormatter('%.1f')
+        self.majorFormatter = FormatStrFormatter('%.2f')
         self.dt_init = dt.datetime(
                 self.stime.year, self.stime.month, self.stime.day)
         self.plot(**kwargs)
-        
-        
+
+
     def plot(self, **kwargs):
         self.fig = plt.figure(figsize=(8.5, 11))
         self.ax_th = self.fig.add_axes(COORD_TH)
@@ -134,14 +136,17 @@ class BlankPlot(object):
             tfmt = '%H:%M:%S000'
         self.ax_th.set_xlim(self.tlim[0], self.tlim[1])
         self.ax_th.xaxis.set_major_formatter(FractionalSecondFormatter(self.ax_th))
-        
+
         # Longitude-Altitue
         self.ax_lon.set_ylabel('Altitude (km MSL)')
         self.ax_lon.set_yticks(self.yticks)
         self.ax_lon.set_ylim(self.zlim)
         self.ax_lon.set_xlim(self.xlim)
         self.ax_lon.minorticks_on()
-        
+        for xlabel_i in self.ax_lon.get_xticklabels():
+            xlabel_i.set_fontsize(0.0)
+            xlabel_i.set_visible(False)
+
         # Height-VHF count
         self.ax_hist.set_xticks([0, 0.1, 0.2, 0.3])
         self.ax_hist.set_yticks(self.yticks)
@@ -149,7 +154,7 @@ class BlankPlot(object):
         self.ax_hist.set_xlim(0, 0.3)
         self.ax_hist.set_xlabel('Freq')
         self.ax_hist.minorticks_on()
-        
+
         # Altitude-Latitude
         self.ax_lat.set_xlabel('Altitude (km MSL)')
         self.ax_lat.set_xticks(self.yticks)
@@ -159,7 +164,7 @@ class BlankPlot(object):
         for xlabel_i in self.ax_lat.get_yticklabels():
             xlabel_i.set_fontsize(0.0)
             xlabel_i.set_visible(False)
-        
+
         # Plan view
         if self.bkgmap==True:
             self.ax_plan.add_feature(COUNTIES, facecolor='none', edgecolor='gray')
@@ -169,50 +174,64 @@ class BlankPlot(object):
         self.ax_plan.set_ylabel('Latitude (degrees)')
         self.ax_plan.set_xlim(self.xlim)
         self.ax_plan.set_ylim(self.ylim)
+
+
+        # lon_formatter = LongitudeFormatter(number_format='.2f',
+        #                                    degree_symbol='',
+        #                                    dateline_direction_label=False)
+        # lat_formatter = LatitudeFormatter(number_format='.2f',
+        #                                   degree_symbol='')
+        # self.ax_plan.xaxis.set_major_formatter(lon_formatter)
+        # self.ax_plan.yaxis.set_major_formatter(lat_formatter)
+        #
         self.ax_plan.minorticks_on()
         self.ax_plan.xaxis.set_major_formatter(self.majorFormatter)
-        self.ax_plan.set_xticks(self.ax_plan.get_xticks())
-        self.ax_plan.set_yticks(self.ax_plan.get_yticks())
+        self.ax_plan.yaxis.set_major_formatter(self.majorFormatter)
+        self.ax_plan.set_xticks(self.ax_lon.get_xticks())
+        self.ax_plan.set_yticks(self.ax_lat.get_yticks())
         if self.bkgmap==True:
-            self.ax_plan.set_extent([self.xlim[0], self.xlim[1], 
+            self.ax_plan.set_extent([self.xlim[0], self.xlim[1],
                                      self.ylim[0], self.ylim[1]])
 
 
 def subplot_labels(plot):
     """
-    Place letters on each subplot panel
+    Place letters on each subplot panel.
+
+    Returns a list of matplotlib text artists
     """
-    plt.text(0.05, 0.8, '(a)', fontsize='x-large', weight='bold',
+    a = plt.text(0.05, 0.8, '(a)', fontsize='x-large', weight='bold',
                      horizontalalignment='center', verticalalignment='center',
                      transform=plot.ax_th.transAxes)
-    plt.text(0.065, 0.80, '(b)', fontsize='x-large', weight='bold',
+    b = plt.text(0.065, 0.80, '(b)', fontsize='x-large', weight='bold',
                      horizontalalignment='center', verticalalignment='center',
                      transform=plot.ax_lon.transAxes)
-    plt.text(0.30, 0.80, '(c)', fontsize='x-large', weight='bold',
+    c = plt.text(0.30, 0.80, '(c)', fontsize='x-large', weight='bold',
                      horizontalalignment='center', verticalalignment='center',
                      transform=plot.ax_hist.transAxes)
-    plt.text(0.065, 0.95, '(d)', fontsize='x-large', weight='bold',
+    d = plt.text(0.065, 0.95, '(d)', fontsize='x-large', weight='bold',
                      horizontalalignment='center', verticalalignment='center',
                      transform=plot.ax_plan.transAxes)
-    plt.text(0.30, 0.95, '(e)', fontsize='x-large', weight='bold',
+    e = plt.text(0.30, 0.95, '(e)', fontsize='x-large', weight='bold',
                      horizontalalignment='center', verticalalignment='center',
                      transform=plot.ax_lat.transAxes)
+    return [a,b,c,d,e]
 
 
 def inset_view(plot, lon_data, lat_data, xlim, ylim, xdiv, ydiv,
                buffer=0.5, inset_size=0.15, plot_cmap = 'magma', bkgmap = True):
     """
-    Overlay an inset panel of size 'inset_size' showing a plan-view histogram 
-    of sources at xdiv, ydiv intervals and outlining a box over xlim and ylim 
+    Overlay an inset panel of size 'inset_size' showing a plan-view histogram
+    of sources at xdiv, ydiv intervals and outlining a box over xlim and ylim
     with buffer of 'buffer' lat/lon degrees in the image.
 
     Add background map features if 'bkgmap' == True
     """
-    inset = plot.fig.add_axes([0.02, 0.01, 0.02+inset_size, 
+    inset = plot.fig.add_axes([0.02, 0.01, 0.02+inset_size,
                               0.01+inset_size],projection=ccrs.PlateCarree())
 
     inset.hist2d(lon_data, lat_data,
-                bins=[np.arange(xlim[0]-buffer, xlim[1]+buffer+xdiv, xdiv), 
+                bins=[np.arange(xlim[0]-buffer, xlim[1]+buffer+xdiv, xdiv),
                       np.arange(ylim[0]-buffer, ylim[1]+buffer+ydiv, ydiv)],
                 density=True, cmap=plot_cmap,
                 cmin=0.00001)
@@ -222,7 +241,8 @@ def inset_view(plot, lon_data, lat_data, xlim, ylim, xdiv, ydiv,
             inset.add_feature(COUNTIES, facecolor='none', edgecolor='gray')
         inset.add_feature(cfeature.BORDERS)
         inset.add_feature(cfeature.STATES.with_scale('10m'))
-        inset.set_extent([xlim[0]-buffer, xlim[1]+buffer, 
+        inset.set_extent([xlim[0]-buffer, xlim[1]+buffer,
                           ylim[0]-buffer, ylim[1]+buffer])
     inset.plot([xlim[0],xlim[0],xlim[1],xlim[1],xlim[0]],
                [ylim[0],ylim[1],ylim[1],ylim[0],ylim[0]],'k')
+    return inset
