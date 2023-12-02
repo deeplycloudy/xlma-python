6b06aa064a6304f2226c449d7a0f30ad16a951e0
Sam Gardner <sam@wx4stg.com>
2023-12-01 19:33:51 -0600

    *sigh*

--- pyxlma/plot/xlma_plot_feature.py
+++ pyxlma/plot/xlma_plot_feature.py
@@ -102,7 +102,7 @@ def plot_points(bk_plot, lon_data, lat_data, alt_data, time_data,
                 fontsize='small', horizontalalignment='left',
                 verticalalignment='center',transform=bk_plot.ax_hist.transAxes)
         # art_hist is a tuple of patch objects. Make it a flat list of artists
-        art_out.extend(art_text)
+        art_out.extend(art_txt)
     art_out.extend(art_hist)
     return art_out
 
