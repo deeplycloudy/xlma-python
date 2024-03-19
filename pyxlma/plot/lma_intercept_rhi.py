113df557eef9918b0a1ba78fc9cfc956b009634a
Sam Gardner <samuel.gardner@ttu.edu>
2024-03-19 14:40:41 -0500

    add docs for lma_intercept_rhi

--- pyxlma/plot/lma_intercept_rhi.py
+++ pyxlma/plot/lma_intercept_rhi.py
@@ -4,8 +4,30 @@ import numpy as np
 
 def rcs_to_tps(radar_latitude, radar_longitude, radar_altitude, radar_azimuth):
     """
-    Given a rhi radar file read by pyart in range elevation azimuth coordinates,
-    it returns the tangent plane coordinates X(pointing east),Y(pointing north),Z(local height) of it.    
+    Find the unit vector coordinates (east, north, up) of the plane of a radar RHI scan.
+    
+    Creates a azimuth, elevation, range and tangent plane cartesian system at the radar's latitude and longitude,
+    and converts the RHI azimuth direction to the tangent plane coordinate system.
+
+    Parameters
+    ----------
+    radar_latitude : `float`
+        Latitude of the radar in degrees.
+    radar_longitude : `float`
+        Longitude of the radar in degrees.
+    radar_altitude : `float`
+        Altitude of the radar in meters.
+    radar_azimuth : `float`
+        Azimuth of the RHI scan in degrees.
+
+    Returns
+    ----------
+    X : `numpy.ndarray`
+        A 1x2 array representing the start and end points eastward component of the RHI scan.
+    Y : `numpy.ndarray`
+        A 1x2 array representing the start and end points northward component of the RHI scan.
+    Z : `numpy.ndarray`
+        A 1x2 array representing the start and end points upward component of the RHI scan.
     """
 
     # Coordinates Systems
@@ -36,14 +58,36 @@ def rcs_to_tps(radar_latitude, radar_longitude, radar_altitude, radar_azimuth):
     return X, Y, Z
 
 
-def geo_to_tps(lma_file, radar_latitude, radar_longitude, radar_altitude):
+def geo_to_tps(lma_file, tps_latitude, tps_longitude, tps_altitude):
     """
-    Given a lma file read by lmatools in latitude, longitude, altitude coordinates,it returns the tangent plane coordinates Xlma(pointing east),Ylma(pointing north),Z(local height) of it.
+    Convert the latitude, longitude, and altitude of LMA VHF sources to x/y/z distances (in meters) from an arbitrary latitude, longitude, and altitude.
+
+    Creates a tangent plane cartesian system at the latitude, longitude, and altitude provided, and converts the LMA VHF sources to the tangent plane coordinate system.
+
+    Parameters
+    ----------
+    lma_file : `xarray.Dataset`
+        A pyxlma dataset containing latitude, longitude, and altitude of LMA VHF sources.
+    tps_latitude : `float`
+        Latitude of the tangent plane in degrees.
+    tps_longitude : `float`
+        Longitude of the tangent plane in degrees.
+    tps_altitude : `float`
+        Altitude of the tangent plane in meters.
+    
+    Returns
+    ----------
+    Xlma : `numpy.ndarray`
+        A 1xN array representing the eastward distance (in meters) of the tangent plane center to the LMA VHF sources.
+    Ylma : `numpy.ndarray`
+        A 1xN array representing the northward distance (in meters) of the tangent plane center to the LMA VHF sources.
+    Zlma : `numpy.ndarray`
+        A 1xN array representing the upward distance (in meters) of the tangent plane center to the LMA VHF sources.
     """
     # GeographicSystem GEO - Lat, lon, alt
     geo = GeographicSystem()
     # Tangent Plane Cartesian System TPS - 
-    tps = TangentPlaneCartesianSystem(radar_latitude, radar_longitude, radar_altitude)
+    tps = TangentPlaneCartesianSystem(tps_latitude, tps_longitude, tps_altitude)
 
     # GEO to TPS
 
@@ -62,8 +106,29 @@ def geo_to_tps(lma_file, radar_latitude, radar_longitude, radar_altitude):
 
 def ortho_proj_lma(lma_file, radar_latitude, radar_longitude, radar_altitude, radar_azimuth):
     """
-    Given a lma file read by lmatools and radar file read by pyart, it transforms both datasets to tangent plane coordinate system,
-    and returns the lma sources coordinates rotated with x:pointing along the RHI scan, y:orthogonal to x, counterclockwise and z:local height.
+    Convert the latitude, longitude, and altitude of LMA VHF sources to distance along, distance from, and height above the ground of a radar RHI scan.
+    
+    Creates tangent plane at the radar's location and converts the LMA VHF sources to the tangent plane coordinate system, then rotates the tangent plane in the direction of the scan azimuth.
+
+    
+    Parameters
+    ----------
+    lma_file : `xarray.Dataset`
+        A pyxlma dataset containing latitude, longitude, and altitude of N number of LMA VHF sources.
+    radar_latitude : `float`
+        Latitude of the radar in degrees.
+    radar_longitude : `float`
+        Longitude of the radar in degrees.
+    radar_altitude : `float`
+        Altitude of the radar in meters.
+    radar_azimuth : `float`
+        Azimuth of the RHI scan in degrees.
+
+    Returns
+    ----------
+    lma_file_loc : `numpy.ndarray`
+        A Nx3 array representing the distance along, distance from, and height above the ground (in m) of the LMA VHF sources.
+
     """
     Xlma,Ylma,Zlma = geo_to_tps(lma_file, radar_latitude, radar_longitude, radar_altitude)
     X, Y, Z = rcs_to_tps(radar_latitude, radar_longitude, radar_altitude, radar_azimuth)
@@ -121,11 +186,48 @@ def ortho_proj_lma(lma_file, radar_latitude, radar_longitude, radar_altitude, ra
     lma_file_loc[:,1] = np.sqrt(lma_file_loc_perp[:,0]**2 + lma_file_loc_perp[:,1]**2)
     lma_file_loc[:,2] = Zlma
 
-
     return lma_file_loc
 
 
-def find_points_near_rhi(lma_file, radar_latitude, radar_longitude, radar_altitude, radar_azimuth, radar_scan_time, distance_threshold=1000, time_threshold=60):
+def find_points_near_rhi(lma_file, radar_latitude, radar_longitude, radar_altitude, radar_azimuth, radar_scan_time, distance_threshold=1000, time_threshold=30):
+    """
+    Find the LMA VHF sources near a radar RHI scan.
+    
+    Creates tangent plane at the radar's location and converts the LMA VHF sources to the tangent plane coordinate system, then rotates the tangent plane in the direction of the scan azimuth.
+    Filters RHI scan points based on distance and time thresholds.
+
+    
+    Parameters
+    ----------
+    lma_file : `xarray.Dataset`
+        A pyxlma dataset containing latitude, longitude, and altitude, and event_id of N number of LMA VHF sources.
+    radar_latitude : `float`
+        Latitude of the radar in degrees.
+    radar_longitude : `float`
+        Longitude of the radar in degrees.
+    radar_altitude : `float`
+        Altitude of the radar in meters.
+    radar_azimuth : `float`
+        Azimuth of the RHI scan in degrees.
+    radar_scan_time : `datetime.datetime` or `numpy.datetime64` or `pandas.Timestamp`
+        Time of the RHI scan.
+    distance_threshold : `float`
+        Maximum distance from the radar to the LMA VHF sources in meters. Default is 1000.
+    time_threshold : `float`
+        Number of seconds before and after the RHI scan time to include LMA VHF sources. Default is 30. (total length: 1 minute)
+
+
+    Returns
+    ----------
+    lma_range : `numpy.ndarray`
+        A 1D array representing the distance along the radar RHI scan to each filtered LMA point.
+    lma_dist : `numpy.ndarray`
+        A 1D array representing the distance from the radar RHI scan plane to each filtered  LMA point.
+    lma_alt : `numpy.ndarray`
+        A 1D array representing the height above the radar of each filtered  LMA point.
+    lma_ids : `numpy.ndarray`
+        A 1D array representing the event_id of each filtered LMA point.
+    """
 
     radar_azimuth = radar_azimuth % 360
 
