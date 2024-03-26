a866c70b004339c9655beaa3495b5a7d2b6b5da4
Sam Gardner <samuel.gardner@ttu.edu>
2024-03-26 12:55:34 -0500

    nldn tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -3,6 +3,8 @@
 from os import listdir
 from datetime import datetime as dt
 import xarray as xr
+import pandas as pd
+import numpy as np
 from pyxlma.lmalib.io import read as lma_read
 from tests.test_flash import compare_dataarrays
 
@@ -22,3 +24,21 @@ def test_read_onefile_dataset():
     truth = xr.open_dataset('tests/truth/lma_netcdf/small_lma.nc')
     for var in truth.data_vars:
         compare_dataarrays(dataset, truth, var)
+
+def test_read_nldn():
+    dataset = lma_read.nldn('examples/network_samples/gld360enldnns_20231224_daily_v1_lit.raw')
+    truth = pd.DataFrame({
+        'latitude' : [33.582, 33.590, 33.585],
+        'longitude' : [-101.881, -102.032, -101.872],
+        'peak_current_kA' : [12.0, -9.0, -15.0],
+        'multiplicity' : [0, 0, 0],
+        'semimajor': [0.4, 0.2, 2.2],
+        'semiminor': [0.2, 0.2, 1.5],
+        'majorminorratio': [1.8, 1.0, 1.5],
+        'ellipseangle' : [75, 3, 26],
+        'chi2' : [0.8, 1.0, 2.1],
+        'num_stations': [4, 5, 6],
+        'type': ['IC', 'CG', 'IC'],
+        'datetime' : [np.datetime64('2023-12-24T00:57:01.123456789'), np.datetime64('2023-12-24T00:57:31.987654321'), np.datetime64('2023-12-24T00:57:58.135792468')]
+    })
+    assert dataset.equals(truth)
